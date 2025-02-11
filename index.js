56e2dd765b8b084d3af35d4e2e79aec0712b7191
Ujjwal-Invact <136787080+Ujjwal-invact@users.noreply.github.com>
2025-02-11 17:36:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// const { createClient } = require('@supabase/supabase-js');
-
-// // Replace with your Supabase details
-// const SUPABASE_URL= 'https://bbtywinqgjripmjsgcjp.supabase.co'
-// const SUPABASE_KEY= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJidHl3aW5xZ2pyaXBtanNnY2pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMzMyMTUsImV4cCI6MjA1MzgwOTIxNX0.ZrwybcnSZWhd-VjiJUzAV5vpX0KVGvWjSv0OrTYqznc'
-
-
-// const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// // Test connection
-// (async () => {
-//     const { data, error } = await supabase.from('leads_collection').select('*');
-    
-//     if (error) {
-//         console.error('Error:', error);
-//     } else {
-//         console.log('Data:', data);
-//     }
-// })();
 
 
 
